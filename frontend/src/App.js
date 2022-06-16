35eaff27837629ba0ccb05355c535669c083e57c
rishitha_gurram <rishichowdary1999@gmail.com>
2022-06-16 14:30:21 +0530

    Added another CMD line in BE docker file.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ const HasAccessToRouter = ({config}) => {
         authTokenType: config.AUTHORIZE_TOKEN_TYPE,
         scopes: config.SCOPES,
         updateCheckText: config.UPDATE_CHECK_TEXT,
-        june02: 'TecVerify 2ndJune 3:15PM'
+        june16: 'TecVerify 16thJune 2:25PM'
     };
     sessionStorage.setItem('authConfig', JSON.stringify(authConfig));
     oktaAuth = new OktaAuth(authConfig);
